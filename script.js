0b3f0e3c035d500a95601f8128d8c7c6b9442310
truongvd05 <truongvdf8@fullstack.edu.vn>
2025-07-06 15:16:51 +0700

    update css

--- script.js
+++ script.js
@@ -17,16 +17,20 @@ async function send(method, url) {
     }
 }
 
-send("GET", `https://dummyjson.com/products/${id}`).then((res) => {
-    const h2 = document.createElement("h2");
-    h2.innerText = `${res.title}`;
-    const description = document.createElement("p");
-    description.innerText = `${res.description}`;
-    const category = document.createElement("p");
-    category.innerText = `${res.category}`;
-    const price = document.createElement("p");
-    price.innerText = `Giá: ${res.price}`;
-    const rating = document.createElement("p");
-    rating.innerText = `Đánh giá: ${res.rating}`;
-    products.append(h2, description, category, price, rating);
-});
+send("GET", `https://dummyjson.com/products/${id}`)
+    .then((res) => {
+        const h2 = document.createElement("h2");
+        h2.innerText = `${res.title}`;
+        const description = document.createElement("p");
+        description.innerText = `${res.description}`;
+        const category = document.createElement("p");
+        category.innerText = `${res.category}`;
+        const price = document.createElement("p");
+        price.innerText = `Giá: ${res.price}`;
+        const rating = document.createElement("p");
+        rating.innerText = `Đánh giá: ${res.rating}`;
+        products.append(h2, description, category, price, rating);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
